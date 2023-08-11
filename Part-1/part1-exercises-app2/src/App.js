313bd22cc941b8354a8bbc8278e2a8d6f545d484
Philip Kim <philipjwkim@gmail.com>
2023-08-10 19:20:05 -0700

    Completion of application 2 consisting of a random anecdotes generator. Utilizes the copying of an array to change the amount of votes for each randomly selected quote.

---
+++ Part-1/part1-exercises-app2/src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [mostVotes, setMostVotes] = useState(0)
+
+  const [votes, setVotes] = useState(anecdotes.map(_ => 0)) //create an array of values 0 of anecdotes length
+
+  const handleClick = () => {
+   setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const handleVote = () => {
+
+      const newVotes = [...votes]   //create copy of votes array with values 0
+      newVotes[selected] += 1
+      setVotes(newVotes)
+      
+      console.log('Incremented copy at ' + selected)
+      console.log('The amount of votes for this quote is ' + votes[selected])
+
+      if(newVotes[selected] > votes[mostVotes]) {
+        setMostVotes(selected)
+      }
+  }
+
+
+
+  return (
+    <>
+    <div>
+    <h2>Anecdote of the day</h2>
+    {anecdotes[selected]}
+    </div>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleClick}>next anecdote</button>
+    <h2>Anecdotes with most votes</h2>
+    <div>
+      {anecdotes[mostVotes]}
+    </div>
+    <div>
+      has {votes[mostVotes]} votes
+    </div>
+
+    </>
+  )
+}
+
+export default App;
