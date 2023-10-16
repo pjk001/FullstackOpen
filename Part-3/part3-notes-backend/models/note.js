94c63122dec84d60eff8fb9e3292bc8ebc0bbfa7
Philip Kim <philipjwkim@gmail.com>
2023-10-16 11:02:01 -0700

    Changes to the follow-along notes application backend.

---
+++ Part-3/part3-notes-backend/models/note.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  important: Boolean,
+})
+
+//even though the _id property of Mongoose objects looks like a string, it is in fact an object
+//the toJSON method we defined tranforms it into a single string just to be safe
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
