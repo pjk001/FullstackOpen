2568120fc700d8b30799976cc537deb7bc07c27b
Philip Kim <philipjwkim@gmail.com>
2023-10-24 15:58:15 -0700

    Added EsLint and fixed all changes to the follow-along backend for the notes application.

--- Part-3/part3-notes-backend/models/note.js
+++ Part-3/part3-notes-backend/models/note.js
@@ -1,30 +1,14 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })
 
-//even though the _id property of Mongoose objects looks like a string, it is in fact an object
-//the toJSON method we defined tranforms it into a single string just to be safe
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
